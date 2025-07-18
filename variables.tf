e677eec0bce9642933f8fd125751537107bf1893
co6tter <co6tter@gmail.com>
2025-07-18 16:22:50 +0900

    feat: add domain and acm

--- variables.tf
+++ variables.tf
@@ -9,3 +9,16 @@ variable "bucket_name_prefix" {
   type        = string
   default     = "c2-learn-s3"
 }
+
+variable "zone_name" {
+  description = "Route 53 Hosted Zone root"
+  type        = string
+  default     = "example.com." # 末尾ドット推奨
+}
+
+# CloudFront で実際に使う “サブドメイン” (例: cdn.example.com)
+variable "domain_name" {
+  description = "FQDN shown to users"
+  type        = string
+  default     = "cdn.example.com"
+}
